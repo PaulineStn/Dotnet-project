81e684f6d90891ad8ddf93303c093df8f3484389
Mahussi DATONGNON <mahussidatongnon@gmail.com>
2025-12-18 09:40:48 +0100

    update SetupService.cs

--- Gauniv.WebServer/Services/SetupService.cs
+++ Gauniv.WebServer/Services/SetupService.cs
@@ -63,6 +63,11 @@ namespace Gauniv.WebServer.Services
                     throw new Exception("ApplicationDbContext is null");
                 }
                 
+                if (applicationDbContext.Database.GetPendingMigrations().Any())
+                {
+                    applicationDbContext.Database.Migrate();
+                }
+                
                  // 1️⃣ Créer le rôle Admin s'il n'existe pas
                 if (!await roleManager.RoleExistsAsync("Admin"))
                 {
@@ -103,6 +108,46 @@ namespace Gauniv.WebServer.Services
                     await userManager.AddToRoleAsync(admin, "Admin");
                 }
                 
+                
+                // Create Category
+                if (!applicationDbContext.Categories.Any())
+                {
+                    applicationDbContext.Categories.AddRange(
+                        new Category { Name = "Action" },
+                        new Category { Name = "RPG" },
+                        new Category { Name = "Indie" }
+                    );
+                    applicationDbContext.SaveChanges(); // pour avoir les Id
+                }
+                
+                if (!applicationDbContext.Games.Any())
+                {
+                    var local_action = applicationDbContext.Categories.First(c => c.Name == "Action");
+                    var local_rpg = applicationDbContext.Categories.First(c => c.Name == "RPG");
+
+                    var local_game1 = new Game
+                    {
+                        Name = "Space Blaster",
+                        Description = "Fast action shooter",
+                        Price = 19.99m,
+                        CurrentVersion = "1.0.0",
+                        Payload = Encoding.UTF8.GetBytes("FAKE_BINARY_GAME_1"),
+                        Categories = new List<Category> { local_action }
+                    };
+
+                    var local_game2 = new Game
+                    {
+                        Name = "Dungeon Quest",
+                        Description = "Classic RPG adventure",
+                        Price = 29.99m,
+                        CurrentVersion = "0.9.1",
+                        Payload = Encoding.UTF8.GetBytes("FAKE_BINARY_GAME_2"),
+                        Categories = new List<Category> { local_rpg }
+                    };
+
+                    applicationDbContext.Games.AddRange(local_game1, local_game2);
+                }
+
                 // 5️⃣ Sauvegarder les changements (au cas où)
                 await applicationDbContext.SaveChangesAsync();
                 
