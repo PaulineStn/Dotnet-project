00f4b48657fc3a897ea90edeb5482418f030b8eb
Mahussi DATONGNON <mahussidatongnon@gmail.com>
2025-12-18 16:14:32 +0100

    add front

--- Gauniv.Client/ViewModel/IndexViewModel.cs
+++ Gauniv.Client/ViewModel/IndexViewModel.cs
@@ -1,19 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using Gauniv.Client.Pages;
-using Gauniv.Client.Services;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Gauniv.Network;
 
 namespace Gauniv.Client.ViewModel
 {
     public partial class IndexViewModel: ObservableObject
     {
+        private readonly ApiClient _api;
+
+        [ObservableProperty]
+        private ObservableCollection<GameDto> games = new();
+
+        public IndexViewModel(ApiClient api)
+        {
+            _api = api;
+            LoadGames();
+        }
+        
+        private void LoadGames()
+        {
+                
+            var req = _api.GetAllAsync(null, null, null, null, null, null);
+            
+            Console.WriteLine($"games: {req.Result}");
+        
+            Games = new ObservableCollection<GameDto>(req.Result);
+            // Games = new ObservableCollection<Game>
+            // {
+            //     new Game { Title = "Jeu 1", ImagePath = "jeu1.jpg" ,Description="lorem ipsum"},
+            //     new Game { Title = "Jeu 2", ImagePath = "jeu2.jpg",Description="lorem ipsum" },
+            // };
+        }
+
+        // [RelayCommand]
+        // private async Task SelectGame(Game game)
+        // {
+        //     // Navigation vers les détails du jeu
+        //     await Shell.Current.GoToAsync($"gamedetails?id={game.Id}");
+        // }
     }
 }
